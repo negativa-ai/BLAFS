7129bb3886c49d0b5a9727abfba745c2ef4a428f
jzh18 <hfzhang.justin@icloud.com>
2025-02-04 20:24:44 +0000

    Implement redirect

--- fs/include/util.h
+++ fs/include/util.h
@@ -3,5 +3,10 @@
 #define BAFFS_FS_UTIL_H_
 
 const char *to_target_path(const char *path, const char *target_dir);
+bool path_exists(const char *path);
+int copy_file(const char *from, const char *to);
+
+// copy file times from src_file to dst_path
+int copy_file_times(struct stat *src_file, const char *dst_path);
 
 #endif // BAFFS_FS_UTIL_H_
--- fs/src/util.cc
+++ fs/src/util.cc
@@ -1,3 +1,6 @@
+#include <utime.h>
+#include <sys/stat.h>
+
 #include <filesystem>
 
 #include "util.h"
@@ -7,4 +10,24 @@ const char *to_target_path(const char *path, const char *target_dir)
     std::filesystem::path lower{target_dir};
     lower /= path;
     return lower.c_str();
+}
+
+bool path_exists(const char *path)
+{
+    struct stat stbuf;
+    return lstat(path, &stbuf) == 0;
+}
+
+int copy_file(const char *from, const char *to, bool retain_time)
+{
+    std::string cmd{"cp -rP \"" + std::string(from) + "\" \"" + std::string(to) + "\""};
+    return system(cmd.c_str());
+}
+
+int copy_file_times(struct stat *src_file, const char *dst_path)
+{
+    struct utimbuf new_times;
+    new_times.actime = src_file->st_atime;
+    new_times.modtime = src_file->st_mtime;
+    return utime(dst_path, &new_times);
 }
